76c61f214cdd6cb4a28372edb49cbe701394d20a
Raj <rajsandhu@gmail.com>
2020-01-22 19:29:03 +0000

    given solution -> english -> my reconstituted solution

--- src/App.js
+++ src/App.js
@@ -104,8 +104,26 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
+
         </header>
+		<ul>
         <h2>Favorite Movies</h2>
+
+          {profiles.map(profile => {
+              const userName = users[profile.userID].name;
+              const favMovieName = movies[profile.favoriteMovieID].name;
+
+              return(
+                  <li key={profile.id}>
+                <p>{`${userName}'s favorite movie is "${favMovieName}."`}</p>
+              </li>
+              );
+            }
+           )
+          }
+
+
+		</ul>
       </div>
     );
   }
